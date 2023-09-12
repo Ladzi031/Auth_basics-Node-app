f741c569cda6763c689db1afda45fc1276bf0ecb
Ladzi <ladziladzani@gmail.com>
2023-09-12 21:37:14 +0200

    setUp express, routes and mongoose

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const passport = require("passport-jwt");
-const moongose = require("mongoose");
+const mongoose = require("mongoose");
+const config = require("./config/database");
 
 const app = express();
 
 const users = require("./routes/users");
+
+mongoose.connect(config.database);
+
+mongoose.connection.on("connected", () => {
+  console.log("connected to the database: " + config.database);
+});
 
 // port Number
 const PORT = 3000;
@@ -17,6 +24,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/users", users);
+
+app.use(express.static(path.join(__dirname, "public")));
 
 //Index Route...
 app.get("/", (req, res) => {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Register...
-router.get('/register', (req, res, next) => {
-    res.send("Register endPoint reached!");
+router.get("/register", (req, res, next) => {
+  res.send("Register!");
+});
+
+router.get("/authenticate", (req, res, next) => {
+  res.send("Authenticate!");
+});
+
+router.get("/profile", (req, res, next) => {
+  res.send("profile!");
+});
+
+
+router.get("/validate", (req, res, next) => {
+  res.send("validate");
 });
 
 module.exports = router;
